edda444efd639862a67b0a866f4e94df348318b0
John De Jesus <Jd@dhcp-v092-064.mobile.uci.edu>
2018-03-03 10:38:57 -0800

    firebase

---
+++ Assets/javascript/js.js
@@ -0,0 +1,15 @@
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAcCsp_1wcbimGlS4lKAtxlNz55p5-HbX4",
+    authDomain: "bootcamp-project-1-47c35.firebaseapp.com",
+    databaseURL: "https://bootcamp-project-1-47c35.firebaseio.com",
+    projectId: "bootcamp-project-1-47c35",
+    storageBucket: "bootcamp-project-1-47c35.appspot.com",
+    messagingSenderId: "692503538126"
+  };
+  firebase.initializeApp(config);
+
+  // datebase Var
+  var database = firebase.database() 
