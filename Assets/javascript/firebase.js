aede3477cb226019d81b5dbfb8c68c4d4b264aa7
John De Jesus <Jd@dhcp-v118-006.mobile.uci.edu>
2018-03-10 11:50:19 -0800

    added some ids for tranparent nav bar

--- Assets/javascript/firebase.js
+++ Assets/javascript/firebase.js
@@ -1,20 +1,27 @@
-var config = {
-    apiKey: "AIzaSyAcCsp_1wcbimGlS4lKAtxlNz55p5-HbX4",
-    authDomain: "bootcamp-project-1-47c35.firebaseapp.com",
-    databaseURL: "https://bootcamp-project-1-47c35.firebaseio.com",
-    projectId: "bootcamp-project-1-47c35",
-    storageBucket: "bootcamp-project-1-47c35.appspot.com",
-    messagingSenderId: "692503538126"
-};
-firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyAcCsp_1wcbimGlS4lKAtxlNz55p5-HbX4",
+//     authDomain: "bootcamp-project-1-47c35.firebaseapp.com",
+//     databaseURL: "https://bootcamp-project-1-47c35.firebaseio.com",
+//     projectId: "bootcamp-project-1-47c35",
+//     storageBucket: "bootcamp-project-1-47c35.appspot.com",
+//     messagingSenderId: "692503538126"
+// };
+// firebase.initializeApp(config);
 
-// datebase Var
-var database = firebase.database()
+// // datebase Var
+// var database = firebase.database()
 
+//register button, need to upload to firebase.
 $("#signUpBtn").on("click", function () {
 var user = $("#registerUser").val()
+
 console.log(user)
+
 var newUser = {
-    user: name,
+    user: user,
+    // myEvents:{
+
+    // }
 }
+
 })
