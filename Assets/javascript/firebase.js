e14e3349346862cbd563ee519ead2d983f22bf08
Mark Chiang <markchiang1@gmail.com>
2018-03-11 10:15:38 -0700

    signup btn clk worked on partially working

--- Assets/javascript/firebase.js
+++ Assets/javascript/firebase.js
@@ -14,53 +14,68 @@ var database = firebase.database()
 var user = ''
 var foundUser = false
 
+
+
+
+// database.ref().on("child_added", function(snapshot){
+    //     var object = snapshot.val()
+    //     console.log(object)
+    // })
+    // object.child('name').off();
+function checkUser(u1){
+    var usersRef = firebase.database().ref(u1+"/")
+    usersRef.orderByChild("name").equalTo(u1).on("child_added", function(snapshot){
+        var object = snapshot.val().name
+        // console.log(u1)
+        console.log(object)
+        if(u1=== object){
+            alert("user in database")
+            console.log("Found Child")
+            foundUser=true
+            console.log(foundUser)  
+            return true
+            location.href="home.html"
+        }
+    })
+}
+
 $("#login").on("click", function (event) {
     event.preventDefault()
     var input = $("#loginUser").val()
     console.log(input)
-    if(input === object){
-        alert("user in database")
-        console.log("Found Child")
-        foundUser =true;
-        $('#user').html(input)
-        location.href="home.html"
-    }
-    else{
-            alert("must sign up")
-            foundUser = false;
-            location.href="signUp.html"
-    }
+
+    // var inDatabase = checkUser(input)
+    // if(inDatabase===false){
+    //     alert("must sign up")
+    //     foundUser = false;
+    //     database.ref("/"+input).push({name:input})
+    // }
+    // else{
+    //     $('#user').html(user)
+    //     location.href="home.html"
+    // }
 })
 
 
-database.ref().on("child_added", function(snapshot){
-    var object = snapshot.val()
-    console.log(object)
-    
-    })
+// playersRef.orderByChild("name").equalTo("John").on("child_added", function(data) {
+//     console.log("Equal to filter: " + data.val().name);
 
-//register button, need to upload to firebase.
-$("#signUpBtn").on("click", function (event) {
+$("#signUpBtn").on("click", function (event){
     event.preventDefault()
     user = $("#registerUser").val().trim()
     console.log(user)
-    if(user === object){
-        alert("user in database")
-        console.log("Found Child")
-        foundUser =true;
-        $('#user').html(user)
-        // location.href="home.html"
+    var inDatabase = checkUser(user)
+    if(inDatabase){
+        alert("That user already exists")
+        alert("imposter")
     }
     else{
-            alert("must sign up")
-            foundUser = false;
-        }
-    // database.ref().orderByChild().equalTo(user){       
+        database.ref("/"+user).push({name:user})
+        alert("added user")
+    }
 })
+    
 
-database.ref("/"+user).push({
-    name:user
-})
 
             
 
